6528e874ec1f70db8b81c154f14f05a82aed4705
martinmosquera <martinmosqueraa@gmail.com>
2022-03-28 03:44:26 -0300

    funcao arv_pertence

--- tree.c
+++ tree.c
@@ -8,16 +8,14 @@ typedef struct arvore {
 } Arvore;
 
 
-Arvore*  cria_arv_vazia (Arvore* a);
+Arvore*  cria_arv_vazia ();
 Arvore*  arv_constroi (char c, Arvore* e, Arvore* d);
 int      verifica_arv_vazia (Arvore* a);
 Arvore*  arv_libera (Arvore* a);
 int      arv_pertence (Arvore* a, char c);
 void     arv_imprime (Arvore* a);
 
-Arvore* cria_arv_vazia (Arvore* a) {
-  a->dir == NULL;
-  a->esq == NULL;
+Arvore* cria_arv_vazia () {
    return NULL;
 }
 
@@ -43,7 +41,6 @@ Arvore* arv_libera (Arvore* a) {
 }
 
 void arv_imprime (Arvore* a){
-	// if(!a) return;
   if(a == NULL) {
     printf(" <> ");
     return;
@@ -54,15 +51,30 @@ void arv_imprime (Arvore* a){
   printf(" > ");  
 }
 
+int arv_pertence(Arvore* a,char c){
+  if(a == NULL) return 0;
+  if(a->info == c) return 1;  
+  if(arv_pertence(a->esq,c)) return 1;
+  if(arv_pertence(a->dir,c)) return 1;
+}
+
 
 int main (int argc, char *argv[]) {
   Arvore *a, *a1, *a2, *a3, *a4, *a5;
-  a1 = arv_constroi('d',cria_arv_vazia(a1),cria_arv_vazia(a1));
-  a2 = arv_constroi('b',cria_arv_vazia(a2),a1);
-  a3 = arv_constroi('e',cria_arv_vazia(a3),cria_arv_vazia(a3));
-  a4 = arv_constroi('f',cria_arv_vazia(a4),cria_arv_vazia(a4));
+  char c;
+  int p;
+  int r = 0;
+  a1 = arv_constroi('d',cria_arv_vazia(),cria_arv_vazia());
+  a2 = arv_constroi('b',cria_arv_vazia(),a1);
+  a3 = arv_constroi('e',cria_arv_vazia(),cria_arv_vazia());
+  a4 = arv_constroi('f',cria_arv_vazia(),cria_arv_vazia());
   a5 = arv_constroi('c',a3,a4);
   a  = arv_constroi('a',a2,a5);
   arv_imprime(a);
+  scanf("%c",&c);
+    p = arv_pertence(a,c);
+    if(p == 1) printf("\n %c p= %d pertence ao arvore\n",c,p);
+    else printf("\n %c P= %d Nao pertence ao arvore\n",c,p);
+  
   return 0;
 }
